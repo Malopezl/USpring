57ab9d6aaa97462b69a8e6c2215e6dfec30d708c
Marvin Lopez <lopez.marvin9@gmail.com>
2022-06-11 00:34:17 -0600

    Aprendiendo sobre Thymeleaf y Spring MVC

---
+++ HolaMundoThymeleaf/src/main/java/gt/com/holamundo/domain/Persona.java
@@ -0,0 +1,15 @@
+package gt.com.holamundo.domain;
+
+import lombok.Data;
+
+/**
+ *
+ * @author malopez
+ */
+@Data
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+}
