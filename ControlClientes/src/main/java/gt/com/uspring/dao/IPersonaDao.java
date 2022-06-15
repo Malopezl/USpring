6a2337b7c4535d0041828b2e81af4d956e9d00b1
Marvin Lopez <lopez.marvin9@gmail.com>
2022-06-14 19:12:28 -0600

    Agregando manejo de usuarios y roles desde mysql y seguridad con JPA

--- ControlClientes/src/main/java/gt/com/uspring/dao/IPersonaDao.java
+++ ControlClientes/src/main/java/gt/com/uspring/dao/IPersonaDao.java
@@ -1,12 +1,12 @@
 package gt.com.uspring.dao;
 
 import gt.com.uspring.domain.Persona;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  *
  * @author malopez
  */
-public interface IPersonaDao extends CrudRepository<Persona, Long> {
+public interface IPersonaDao extends JpaRepository<Persona, Long> {
 
 }
--- ControlClientes/src/main/java/gt/com/uspring/web/SecurityConfig.java
+++ ControlClientes/src/main/java/gt/com/uspring/web/SecurityConfig.java
@@ -2,11 +2,14 @@ package gt.com.uspring.web;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  *
@@ -16,17 +19,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configurerGlobal(AuthenticationManagerBuilder build) {
         try {
-            auth.inMemoryAuthentication()
-                    .withUser("admin")
-                    .password("{noop}123")
-                    .roles("ADMIN", "USER")
-                    .and()
-                    .withUser("user")
-                    .password("{noop}123")
-                    .roles("USER");
+            build.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
         } catch (Exception ex) {
             Logger.getLogger(SecurityConfig.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -36,14 +35,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) {
         try {
             http.authorizeRequests()
-                    .antMatchers("/editar/**", "/agregar/**", "/eliminar")
-                    .hasRole("ADMIN")
-                    .antMatchers("/")
-                    .hasAnyRole("ADMIN", "USER")
-                    .and()
-                    .formLogin().loginPage("/login")
-                    .and()
-                    .exceptionHandling().accessDeniedPage("/errores/403");
+                .antMatchers("/editar/**", "/agregar/**", "/eliminar")
+                .hasRole("ADMIN")
+                .antMatchers("/")
+                .hasAnyRole("ADMIN", "USER")
+                .and()
+                .formLogin().loginPage("/login")
+                .and()
+                .exceptionHandling().accessDeniedPage("/errores/403");
         } catch (Exception ex) {
             Logger.getLogger(SecurityConfig.class.getName()).log(Level.SEVERE, null, ex);
         }
